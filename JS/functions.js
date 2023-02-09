4a9852b24451988cdea794bfde5fa01d68c0cfcf
Andrés León <andresleoncdm2019@gmail.com>
2023-02-09 03:26:40 -0300

    Primer escritura en el repositorio

---
+++ JS/functions.js
@@ -0,0 +1,13 @@
+    document.getElementById('boton').onclick = function(){
+    
+    console.log ('capturamos el evento onclick' )
+
+    document.getElementById("demo").innerHTML ="NACIÓ: 04/10/95. EDAD:27 D.N.I:39.360.147"
+
+} 
+    document.getElementById("myBtn").onclick = function() {myFunction()};
+
+    function myFunction() {
+     
+    document.getElementById("myDropdown").classList.toggle("show");
+}
